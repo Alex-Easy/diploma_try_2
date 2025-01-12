052ff6a054e69e201914104e814ffcf417ad0d24
Alex Legkovsky <legkovsky.alexei@yandex.ru>
2025-01-12 17:12:33 +0300

    added requirements and some options for django admin pannel

--- procurement/admin.py
+++ procurement/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'date_joined')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('email',)
